3adbd52dffadcb23fb53345ab2107084e4487a1c
Saint. Clever <parchment@gmx.com>
2024-12-05 13:07:26 -0500

    Add and update comments

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import os, pprint
+import os
 import requests
 
 
-# # Request made to API
-# response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
+# Request made to API
+response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
 
-# # Format response and store in list
-# codemaker = response.text.replace("\n", " ").split()
-codemaker = ["0", "1", "3", "5"]
+# Format response and store in list
+codemaker = response.text.replace("\n", " ").split()
+
+# Game test array
+# codemaker = ["0", "1", "3", "5"]
 
 
 # Game initializes
@@ -28,15 +30,6 @@ def game(game_loop):
     # User Input
     user_input = input("Please guess 4 random numbers: ")
     codebreaker = user_input.split()
-
-    data = f'''
-    Example Run:
-    Game initializes and selects “0 1 3 5”
-    Player guesses 2 2 4 6 game responds “all incorrect”
-    Player guesses 0 2 4 6 game responds “1 correct number and 1 correct location”
-    Player guesses 2 2 1 1 game responds “1 correct number and 0 correct location”
-    Player guesses 0 1 5 6 game responds “3 correct numbers and 2 correct location”
-    '''
 
     all_incorrect = ""
     correct_number = ""
@@ -66,6 +59,7 @@ def game(game_loop):
         if len(correct_number_and_location) > 1:
           correct_number_and_location_list = list(correct_number_and_location)
 
+    # # For testing output
     # print("a", all_incorrect)
     # print("b", correct_number)
     # print("c", correct_location)
@@ -76,7 +70,7 @@ def game(game_loop):
     # print("g", correct_location_list)
     # print("h", correct_number_and_location_list)
 
-
+    # Clear terminal
     os.system('cls' if os.name == 'nt' else 'clear')
 
     if all_incorrect_list:
@@ -92,6 +86,7 @@ def game(game_loop):
     elif correct_number_and_location_list:
       print(f"c: {len(correct_number_and_location_list)} correct number and {len(correct_number_and_location_list)} correct location")
     
+    # Add game attempts and attempt count
     game_attempts += f"\n{"".join(user_input)}"
     attempt_count += 1
 
