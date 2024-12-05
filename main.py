6f51038b14c9c924051d54b75016fe59825903a4
Saint. Clever <parchment@gmx.com>
2024-12-05 12:52:13 -0500

    Add minor visuals

--- main.py
+++ main.py
@@ -1,104 +1,100 @@
+import os, pprint
 import requests
 
 
 # # Request made to API
-# response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
+response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
 
 # # Format response and store in list
-# codemaker = response.text.replace("\n", " ").split()
-codemaker = ["0", "1", "3", "5"]
-
-# Grid
-print(f"\n ==== {codemaker} ==== \n")
-
-row_count = 10
-pin = "\u26AB"
-grid = row_count * f"\n| {pin} {pin} {pin} {pin} |"
-print(grid)
-
+codemaker = response.text.replace("\n", " ").split()
+# codemaker = ["0", "1", "3", "5"]
 
 
 # Game initializes
-def game():
-  user_input = input("\n\nPlease guess 4 random numbers: ")
-  codebreaker = user_input.split()
+def game(game_loop):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  attempt_count = 1
+  game_attempts = ""
 
+  # Grid
+  while game_loop != 0:
+    pin = "\u26AB"
+    grid = game_loop * f"\n| {pin} {pin} {pin} {pin} |"
+    print(grid)
+    print(game_attempts)
+    game_loop -= 1
 
-  print(len(codebreaker))
-  print(codemaker, codebreaker)
+    print(f"\n{codemaker}\n")
 
+    # User Input
+    user_input = input("Please guess 4 random numbers: ")
+    codebreaker = user_input.split()
 
-  data = f'''
-  Example Run:
-  Game initializes and selects “0 1 3 5”
-  Player guesses 2 2 4 6 game responds “all incorrect”
-  Player guesses 0 2 4 6 game responds “1 correct number and 1 correct location”
-  Player guesses 2 2 1 1 game responds “1 correct number and 0 correct location”
-  Player guesses 0 1 5 6 game responds “3 correct numbers and 2 correct location”
-  '''
+    data = f'''
+    Example Run:
+    Game initializes and selects “0 1 3 5”
+    Player guesses 2 2 4 6 game responds “all incorrect”
+    Player guesses 0 2 4 6 game responds “1 correct number and 1 correct location”
+    Player guesses 2 2 1 1 game responds “1 correct number and 0 correct location”
+    Player guesses 0 1 5 6 game responds “3 correct numbers and 2 correct location”
+    '''
 
-  game_attempts = []
+    all_incorrect = ""
+    correct_number = ""
+    correct_location = ""
+    correct_number_and_location = ""
 
-  all_incorrect = ""
-  correct_number = ""
-  correct_location = ""
-  correct_number_and_location = ""
+    all_incorrect_list = []
+    correct_number_list = []
+    correct_location_list = []
+    correct_number_and_location_list = []
 
-  all_incorrect_list = []
-  correct_number_list = []
-  correct_location_list = []
-  correct_number_and_location_list = []
+    for i in range(len(codemaker)):
+      if codebreaker[i] != codemaker[i] and codebreaker[i] not in codemaker:
+        all_incorrect += codebreaker[i]
+        
+        if len(all_incorrect) == len(codemaker):
+          all_incorrect_list = list(all_incorrect)
 
-  for i in range(len(codemaker)):
-    if codebreaker[i] != codemaker[i] and codebreaker[i] not in codemaker:
-      all_incorrect += codebreaker[i]
-      
-      if len(all_incorrect) == len(codemaker):
-        all_incorrect_list = list(all_incorrect)
+      elif codebreaker[i] in codemaker and codebreaker[i] != codemaker[i]:
+        correct_number += codebreaker[i]
+        correct_number_list = list(correct_number)
 
-    if codebreaker[i] == codemaker[i]:
-      correct_number_and_location += codebreaker[i]
-      correct_number_and_location_list = list(correct_number_and_location)
-
-      if len(correct_number_and_location) > 1:
+      elif codebreaker[i] == codemaker[i]:
+        correct_number_and_location += codebreaker[i]
         correct_number_and_location_list = list(correct_number_and_location)
 
-    elif codebreaker[i] in codemaker and codebreaker[i] != codemaker[i]:
-      correct_number += codebreaker[i]
-      correct_number_list = list(correct_number)
+        if len(correct_number_and_location) > 1:
+          correct_number_and_location_list = list(correct_number_and_location)
+
+    # print("a", all_incorrect)
+    # print("b", correct_number)
+    # print("c", correct_location)
+    # print("d", correct_number_and_location)
+
+    # print("e", all_incorrect_list)
+    # print("f", correct_number_list)
+    # print("g", correct_location_list)
+    # print("h", correct_number_and_location_list)
 
 
-  print("a", all_incorrect)
-  print("b", correct_number)
-  print("c", correct_location)
-  print("d", correct_number_and_location)
+    
 
-  print("e", all_incorrect_list)
-  print("f", correct_number_list)
-  print("g", correct_location_list)
-  print("h", correct_number_and_location_list)
+    if all_incorrect_list:
+      print("all incorrect")
+      game_attempts += f"\n{", ".join(all_incorrect_list)}"
 
-  if all_incorrect_list:
-    print("all incorrect")
+    elif correct_number_list and correct_number_and_location_list:
+      print(f"a: {len(correct_number_list) + len(correct_number_and_location_list)} correct numbers and {len(correct_number_and_location_list)} correct location")
 
-  elif correct_number_list and correct_number_and_location_list:
-    print(f"LLL {len( correct_number_list) + len( correct_number_and_location_list)} correct numbers and {len(correct_number_and_location_list)} correct location")
+    elif correct_number_list:
+      # correct_number_list[0] should all have the same number so I only need the first one
+      print(f"b: {len(correct_number_list[0])} correct number and {len(correct_number_and_location_list)} correct location")
 
-  elif correct_number_list:
-    # correct_number_list[0] should all have the same number so I only need the first one
-    print(f"LL {len(correct_number_list[0])} correct number and {len(correct_number_and_location_list)} correct location")
-
-  elif correct_number_and_location_list:
-    print(f"L {len(correct_number_and_location_list)} correct number and {len(correct_number_and_location_list)} correct location")
-
-
-  # return f"{codebreaker} all incorrect"
-  # return f"{codebreaker} 1 correct number and 1 correct location"
-  # return f"{codebreaker} 1 correct number and 0 correct location"
-  # return f"{codebreaker} 3 correct number and 2 correct location"
-  
-
-
-
-for i in range(1):
-  print(game())
+    elif correct_number_and_location_list:
+      print(f"c: {len(correct_number_and_location_list)} correct number and {len(correct_number_and_location_list)} correct location")
+    
+    attempt_count += 1
+# Game function with count being the length of game
+count = 4
+game(count)
