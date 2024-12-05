fbe07aea37ff9d81343e3dc4ca8975fe9429b4df
Saint. Clever <parchment@gmx.com>
2024-12-04 22:49:32 -0500

    Create grid, start game logic

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import requests
 
-response = requests.get("https://www.random.org/integers/?num=4&min=1&max=6&col=1&base=10&format=plain&rnd=new")
 
-code = response.text.replace("\n", " ").split()
+# Request made to API
+response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
 
-print(code)
+# Format response and store in list
+codemaker = response.text.replace("\n", " ").split()
+
+
+# Grid
+row_count = 10
+pin = "\u26AB"
+grid = row_count * f"\n| {pin} {pin} {pin} {pin}|"
+print(grid)
+
+
+
+print(f"\n === Game initialized: {codemaker} === \n")
+
+
+
+# Initialize game
+def game():
+  user_input = input("Please insert 4 random numbers: ")
+  codebreaker = user_input.split()
+
+
+  print(len(codebreaker))
+  print(codemaker, codebreaker)
+
+
+  codebreaker_attempts = []
+
+  for i in range(len(codemaker)):
+    if codemaker[i] == codebreaker[i]:
+      codebreaker.append("BLACK")
+
+
+
+
+
+
+for i in range(2):
+  game()
