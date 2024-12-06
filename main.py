152cbe9621198ba34bc0bfde525f43969df161f7
Saint. Clever <parchment@gmx.com>
2024-12-05 21:29:47 -0500

    Add and update comments

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import requests
 # response = requests.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
 
 # Format response and store in list
-# codemaker = response.text.replace("\n", " ").split()
+# cipher = response.text.replace("\n", " ").split()
 
 # Game test array
-codemaker = ["0", "1", "3", "5"]
+cipher = ["0", "1", "3", "5"]
 
 
 # Game initializes
 def game(game_loop):
+  # Number of left attempts to win
   attempt_count = 1
+  # The previously gueesed numbers
   game_attempts = ""
 
   # Grid
@@ -26,7 +28,7 @@ def game(game_loop):
     print(game_attempts)
     game_loop -= 1
 
-    print(f"\n{codemaker}\n")
+    print(f"\n{cipher}\n")
     print("To end game type: exit")
 
     # User Input
@@ -37,13 +39,14 @@ def game(game_loop):
       if user_input == "exit":
         break
 
-      if len(user_input) != 7 or " " not in user_input or int not in user_input:
+      # Check lenght of input, spaces provided, if input is converted is it an int
+      if len(user_input) != 7 or " " not in user_input or int != type(int(user_input.replace(" ", ""))):
         print(f"{user_input} is incorrect. Please try again. Ex: 8 6 7 5")
         game(game_loop)
     except:
       print("Game starting over")
 
-    codebreaker = user_input.split()
+    player_guess = user_input.split()
 
     all_incorrect = ""
     correct_number = ""
@@ -55,21 +58,25 @@ def game(game_loop):
     correct_location_list = []
     correct_number_and_location_list = []
 
-    for i in range(len(codemaker)):
-      if codebreaker[i] != codemaker[i] and codebreaker[i] not in codemaker:
-        all_incorrect += codebreaker[i]
+    for i in range(len(cipher)):
+      # Logic corresponsed to having all entered numbers incorrect
+      if player_guess[i] != cipher[i] and player_guess[i] not in cipher:
+        all_incorrect += player_guess[i]
         
-        if len(all_incorrect) == len(codemaker):
+        if len(all_incorrect) == len(cipher):
           all_incorrect_list = list(all_incorrect)
 
-      elif codebreaker[i] in codemaker and codebreaker[i] != codemaker[i]:
-        correct_number += codebreaker[i]
+      # Logic for having a correct number in the cipher but in the incorrect location
+      elif player_guess[i] in cipher and player_guess[i] != cipher[i]:
+        correct_number += player_guess[i]
         correct_number_list = list(correct_number)
 
-      elif codebreaker[i] == codemaker[i]:
-        correct_number_and_location += codebreaker[i]
+      # logic for matching the the correction number location
+      elif player_guess[i] == cipher[i]:
+        correct_number_and_location += player_guess[i]
         correct_number_and_location_list = list(correct_number_and_location)
 
+        # however if the locations or greater than one that eventually means the number matches as well?
         if len(correct_number_and_location) > 1:
           correct_number_and_location_list = list(correct_number_and_location)
 
