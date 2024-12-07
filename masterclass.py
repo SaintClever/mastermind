24b2cdb5ec6d61c15daa8ca107da075f31f3d973
Saint. Clever <parchment@gmx.com>
2024-12-07 03:22:02 -0500

    Add request and clear screen

--- masterclass.py
+++ masterclass.py
@@ -9,8 +9,16 @@ class Mastermind():
     self.pin = "\u2686"
     self.attempts = []
 
+  def request(self):
+    # response = request.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
+    # cipher = response.text.replace("\n", " ").split()
+    # return cipher
+    
+    cipher = ["0", "1", "3", "5"]
+    return cipher
 
-
+  def clear_screen(self):
+     os.system('cls' if os.name == 'nt' else 'clear')
 
   
 if __name__ == "__main__":
