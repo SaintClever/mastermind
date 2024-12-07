06d57806d4543123703bbc244b2f22d3bf38bef9
Saint. Clever <parchment@gmx.com>
2024-12-07 03:13:40 -0500

    Try to create with classes

---
+++ masterclass.py
@@ -0,0 +1,18 @@
+import os
+import requests
+from rich.console import Console
+
+class Mastermind():
+  # set up
+  def __init__(self, game_loop = 10):
+    self.console = Console
+    self.pin = "\u2686"
+    self.attempts = []
+
+
+
+
+  
+if __name__ == "__main__":
+  game = Mastermind()
+  print(game)
